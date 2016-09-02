9b5ad0437076f8f411052f63cf280bcb1816c1fc
zakooz <marcotribosklol@gmail.com>
2016-09-02 01:56:50 +0100

    n1a touchscreen: disable spammy debug logging if CONFIG_SAMSUNG_PRODUCT_SHIP
    this is consistent with mxtt.c and mxts.c. Without this change, this code is constantly called when a finger is pressed/released/(moved?) which causes unnecessary load on the device and might ramp it away from the lowest cpufreqs
    
    Change-Id: I28ccbb1ae9c019eabb7f4b68ebcd71a6eac13c94

--- drivers/input/touchscreen/mxts_n1.c
+++ drivers/input/touchscreen/mxts_n1.c
@@ -635,14 +635,11 @@ static void mxt_report_input_data(struct mxt_data *data)
 		report_count++;
 
 		if (data->fingers[i].state == MXT_STATE_PRESS) {
+#if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
 			tsp_debug_info(true, &data->client->dev, "[P][%d]: T[%d][%d] X[%d],Y[%d],W[%d],Z[%d],COMP[%d],SUM[%d],P[%d]\n",
 				i, data->fingers[i].type,
 				data->fingers[i].event,
-#if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
 				data->fingers[i].x, data->fingers[i].y,
-#else
-				0,0,
-#endif
 				data->fingers[i].w, data->fingers[i].z, 
 #if TSP_USE_SHAPETOUCH
 				data->fingers[i].component,
@@ -661,16 +658,14 @@ static void mxt_report_input_data(struct mxt_data *data)
 #if TSP_BOOSTER
 			booster_restart = true;
 #endif
+#endif /*CONFIG_SAMSUNG_PRODUCT_SHIP*/
 		}
-		else if (data->fingers[i].state == MXT_STATE_RELEASE)
+		else if (data->fingers[i].state == MXT_STATE_RELEASE) {
+#if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
 			tsp_debug_info(true, &data->client->dev, "[R][%d]: T[%d][%d] X[%d],Y[%d],W[%d],Z[%d],COMP[%d],SUM[%d],P[%d],M[%d]\n",
 				i, data->fingers[i].type,
 				data->fingers[i].event,
-#if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
 				data->fingers[i].x, data->fingers[i].y,
-#else
-				0,0,
-#endif
 				data->fingers[i].w, data->fingers[i].z, 
 #if TSP_USE_SHAPETOUCH
 				data->fingers[i].component,
@@ -685,6 +680,8 @@ static void mxt_report_input_data(struct mxt_data *data)
 				0,0,0
 #endif /*TSP_USE_SHAPETOUCH*/
 			, data->fingers[i].mcount);
+#endif /*CONFIG_SAMSUNG_PRODUCT_SHIP*/
+		}
 
 
 		if (data->fingers[i].state == MXT_STATE_RELEASE) {
