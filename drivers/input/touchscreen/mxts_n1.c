0dc7382375d39950c8468d04948967410d136bc8
Martin Bouchet <tincho5588@gmail.com>
2017-07-08 02:46:51 -0300

    input: unify the sysfs to enable/disable touchscreen booster
    This will create a new node called tsp_booster_enabled in
    /sys/class/sec/tsp
    Echoing 1 to the node will enable the touchscreen booster,
    echoing 0 will disable it.
    
    Applies for devices using synaptics_rmi, synaptics_dsx, mxtt,
    and mxts touchscreens
    
    Change-Id: I490015e0c7cbfcaca84a07318e7ea1bdde53810e

--- drivers/input/touchscreen/mxts_n1.c
+++ drivers/input/touchscreen/mxts_n1.c
@@ -32,9 +32,7 @@
 #endif
 
 #ifdef TSP_BOOSTER
-static unsigned int TSP_BOOSTER_ENABLED = 1;
-
-module_param_named(tsp_booster_enabled, TSP_BOOSTER_ENABLED, uint, S_IWUSR | S_IRUGO);
+static unsigned int tsp_booster_enabled = 1;
 #endif
 
 #ifdef TOUCHKEY_BOOSTER
@@ -759,7 +757,7 @@ static void mxt_report_input_data(struct mxt_data *data)
 	/* all fingers are released */
 	if (count == 0) {
 		mxt_set_dvfs_lock(data, TSP_BOOSTER_OFF, false);
-	} else if (TSP_BOOSTER_ENABLED == 1) {
+	} else {
 		mxt_set_dvfs_lock(data, TSP_BOOSTER_ON, booster_restart);
 	}
 #endif
